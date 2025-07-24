d12a40ac0f5d352702f86aa95c15f30b24b8a3d8
Ian McFarlane <ian.mcfarlane@kdab.com>
2025-07-24 17:54:34 -0400

    switch back to cmake

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 int main()
 {
     CXIndex index = clang_createIndex(0, 0);
-    CXTranslationUnit unit = clang_parseTranslationUnit(index, "file.cpp", nullptr, 0, nullptr, 0, CXTranslationUnit_None);
+    CXTranslationUnit unit = clang_parseTranslationUnit(
+        index, "src/main.cpp", nullptr, 0, nullptr, 0, CXTranslationUnit_None);
 
     if (unit == nullptr) {
         std::cerr << "Unable to parse translation unit. Quitting.\n";
@@ -12,4 +13,24 @@ int main()
     }
 
     CXCursor cursor = clang_getTranslationUnitCursor(unit);
+
+    clang_visitChildren(
+        cursor, // Root cursor
+        [](CXCursor current_cursor, CXCursor parent, CXClientData client_data) {
+            CXString current_display_name =
+                clang_getCursorDisplayName(current_cursor);
+            // Allocate a CXString representing the name of the current cursor
+
+            std::cout << "Visiting element "
+                      << clang_getCString(current_display_name) << "\n";
+            // Print the char* value of current_display_name
+
+            clang_disposeString(current_display_name);
+            // Since clang_getCursorDisplayName allocates a new CXString, it
+            // must be freed. This applies to all functions returning a CXString
+
+            return CXChildVisit_Recurse;
+        },      // CXCursorVisitor: a function pointer
+        nullptr // client_data
+    );
 }
